1136873ee4481efc2dac77c7deebbfeb0a3ffef7
emlai <emil.laine@cs.helsinki.fi>
2016-10-18 14:29:42 +0300

    Add "pkgConfig" and "providers" to Package.swift, remove hardcoded paths

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "CSDL2"
+    name: "CSDL2",
+    pkgConfig: "sdl2",
+    providers: [
+        .Apt("libsdl2-dev"),
+        .Brew("sdl2"),
+    ]
 )
